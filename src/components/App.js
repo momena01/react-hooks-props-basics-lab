701711bf53ea52c53f7c8d4cfeb515b949ad9a2e
momena <momena.salloum59@gmail.com>
2023-11-22 01:32:07 +0300

    Done with assignment

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,13 @@ import user from "../data/user";
 console.log(user);
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+    return (
+        <div>
+            <NavBar />
+            <Home color={user.color} name={user.name} city={user.city} />
+            <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} />
+        </div>
+    );
 }
 
 export default App;
